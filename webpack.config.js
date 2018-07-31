345411517dd9f260f721c7e720e570618c58d8a1
BG <deadpihto21@gmail.com>
2018-07-31 13:28:37 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: "./app/Main.js",
+  output: {
+    filename: "public/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
